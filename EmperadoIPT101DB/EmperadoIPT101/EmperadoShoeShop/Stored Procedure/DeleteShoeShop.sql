091f6ca43e6b7c27438b85df0888a41f274f6952
Emperado7 <144863323+Emperado7@users.noreply.github.com>
2025-10-08 22:09:34 -0930

    Add files via upload

---
+++ EmperadoIPT101DB/EmperadoIPT101/EmperadoShoeShop/Stored Procedure/DeleteShoeShop.sql
@@ -0,0 +1,6 @@
+﻿CREATE PROCEDURE [dbo].[DeleteShoeShop]
+	@ShoeId NVARCHAR(40) = NULL
+AS
+BEGIN
+	DELETE FROM [dbo].[ShoeShop] WHERE ShoeId = @ShoeId;
+END
---
+++ EmperadoIPT101DB/EmperadoIPT101/EmperadoShoeShop/Stored Procedure/GetShoeShop.sql
@@ -0,0 +1,6 @@
+﻿CREATE PROCEDURE [dbo].[GetShoeShop]
+	@ShoeId NVARCHAR(40) = NULL
+AS
+BEGIN
+	SELECT * FROM [dbo].[ShoeShop] AS a WHERE a.[ShoeId] = @ShoeId;
+END
