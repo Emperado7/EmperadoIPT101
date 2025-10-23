bd5550ee9abbefc6967a42fbc5a1938909a7cf05
Emperado7 <144863323+Emperado7@users.noreply.github.com>
2025-10-22 14:30:26 -0930

    Add files via upload

---
+++ EmperadoWindow/EmperadoMainWindow/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace EmperadoApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Button was Tuslok!", "Ang Pogi Mo", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
